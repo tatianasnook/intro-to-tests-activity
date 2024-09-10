a6aee0af6757e89c32171ffbf47678ef5dfe5dfc
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2024-09-10 17:54:32 -0500

    Add README, Testing Instructions and Demo Tests

---
+++ main.py
@@ -0,0 +1,12 @@
+def count_a_letter(sentence, letter):
+    if not letter.isalpha():
+        return None
+    if not sentence:
+        return None
+    
+    count = 0
+    for char in sentence:
+        if char == letter:
+            count +=1
+    
+    return count
